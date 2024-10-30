74ae945caf2742c1cb829159de2a0fc020675a34
Gabriel Malosto <professional@gabdumal.com>
2024-10-30 13:28:10 -0300

    Create environment for Typst

---
+++ environments/typst/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A Flake for a Typst development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell.override
+          { }
+          {
+            packages = with pkgs; [
+              typstyle
+              typst
+              tinymist
+            ];
+          };
+      });
+    };
+}
