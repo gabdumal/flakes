f3439a57dcdaa2be4b6c0e033c53c29c9578c45e
Gabriel Malosto <professional@gabdumal.com>
2024-12-15 16:18:57 -0300

    Enable openssh

--- pure/system/programs.nix
+++ pure/system/programs.nix
@@ -10,13 +10,13 @@
       enable = true;
     };
 
-    ssh = {
-      knownHosts = {
-        "github.com" = {
-          publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
-        };
-      };
-    };
+    # ssh = {
+    #   knownHosts = {
+    #     "github.com" = {
+    #       publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+    #     };
+    #   };
+    # };
 
     zsh = {
       enable = true;
