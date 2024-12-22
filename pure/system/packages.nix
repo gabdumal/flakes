d0bf80c3c8a87c5c8d78e32702d2824a78fc5633
Gabriel Malosto <professional@gabdumal.com>
2024-12-22 04:40:02 -0300

    Remove core packages

--- pure/system/packages.nix
+++ pure/system/packages.nix
@@ -4,7 +4,10 @@
   environment.gnome.excludePackages = with pkgs; [
     gnome-connections
     gnome-console
+    gnome-tour
   ];
+
+  services.xserver.excludePackages = [ pkgs.xterm ];
 
   fonts.packages = with pkgs; [
     montserrat
