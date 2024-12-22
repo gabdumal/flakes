fb486d272062e7c109a4d22ac08cf2675d945938
Gabriel Malosto <professional@gabdumal.com>
2024-12-22 05:05:40 -0300

    Fix update script

--- custom/update.sh
+++ custom/update.sh
@@ -1,5 +1,7 @@
+cd  ~/.dotfiles/nixos/custom
 nix flake update
 git add .
 git commit -m "Update packages"
-home-manager switch -b backup --flake ~/.dotfiles/nixos/custom#custom
 sudo nixos-rebuild switch --flake ~/.dotfiles/nixos/custom#custom
+home-manager switch -b backup --flake ~/.dotfiles/nixos/custom#custom
+cd -
