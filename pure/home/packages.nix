05f4f788c4e512b15b38e1f967ca74fad4384df8
Gabriel Malosto <professional@gabdumal.com>
2025-01-26 16:47:59 -0300

    Move all packages to Home

--- pure/home/packages.nix
+++ pure/home/packages.nix
@@ -7,6 +7,47 @@
     ## Fonts
     nerd-fonts.fira-code
     nerd-fonts.meslo-lg
+
+    ## GNOME
+    nautilus-python
+
+    ## Multimedia
+    ffmpeg
+    imagemagick
+    qbittorrent
+
+    ## Nix
+    appimage-run
+    nil
+    nixpkgs-fmt
+
+    ## Office
+    libreoffice
+
+    ## Programming
+    vscode-fhs
+    zed-editor
+
+    ## Shell
+    fastfetch
+    fzf
+
+    ## System
+    adw-gtk3 # Port GTK3 apps to libadwaita
+    bluez # Bluetooth
+    bluez-tools # Bluetooth tools
+    curl # HTTP client
+    ghostty # Terminal emulator
+    menulibre # Menu editor
+    usbutils # USB tools
+    wget # HTTP client
+
+    ## Web
+    (pkgs.wrapFirefox
+      (pkgs.firefox-unwrapped.override {
+        pipewireSupport = true;
+      })
+      { })
   ];
 
 }
--- pure/system/packages.nix
+++ pure/system/packages.nix
@@ -15,46 +15,6 @@
   ];
 
   environment.systemPackages = with pkgs; [
-    ## GNOME
-    nautilus-python
-
-    ## Multimedia
-    ffmpeg
-    imagemagick
-    qbittorrent
-
-    ## Nix
-    appimage-run
-    nil
-    nixpkgs-fmt
-
-    ## Office
-    libreoffice
-
-    ## Programming
-    vscode-fhs
-    zed-editor
-
-    ## Shell
-    fastfetch
-    fzf
-
-    ## System
-    adw-gtk3 # Port GTK3 apps to libadwaita
-    bluez # Bluetooth
-    bluez-tools # Bluetooth tools
-    curl # HTTP client
-    ghostty # Terminal emulator
-    menulibre # Menu editor
-    usbutils # USB tools
-    wget # HTTP client
-
-    ## Web
-    (pkgs.wrapFirefox
-      (pkgs.firefox-unwrapped.override {
-        pipewireSupport = true;
-      })
-      { })
   ];
 
 }
